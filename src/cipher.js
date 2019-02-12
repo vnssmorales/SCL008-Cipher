e3c69ff9b08e54a8511c42c8a4d84c67ffe86e74
vnssmorales <vnss.morales@gmail.com>
2019-02-12 10:50:41 -0500

    modifico funciones cipher y descipher

--- src/cipher.js
+++ src/cipher.js
@@ -3,16 +3,23 @@ window.cipher = {
     let result ="";
     let concatenar="";
     let transform_text=0;
-    for (let i=0;i<text.length;i++){
-    transform_text=text.charCodeAt(i);
-    concatenar=String.fromCharCode((transform_text-65+offset)%26+65);
+    for (let i=0;i<encode.length;i++){
+    transform_text=encode.charCodeAt(i);
+    concatenar=String.fromCharCode((transform_text-65+parseInt(offset_value))%26+65);
     result+= concatenar
     
     }
     return result;
-    }
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (offset_value,decode) => {
+    let result ="";
+    let concatenar="";
+    let transform_text=0;
+    for (let i=0;i<decode.length;i++){
+    transform_text=decode.charCodeAt(i);
+    concatenar=String.fromCharCode((transform_text-90+parseInt(offset_value))%26+90);
+    result+= concatenar
   }
-};
+   return result;
+  } 
+ };
--- src/index.js
+++ src/index.js
@@ -10,10 +10,14 @@ button_enter.addEventListener('click', () =>{
 button_cipher.addEventListener('click', () =>{
    document.getElementById("box2").style.display="none";
    document.getElementById("box3").style.display="block";
+
+   let offset_value = document.getElementById('offset').value;
    let encode = document.getElementById('codificar').value;
    document.getElementById('codificar').value = "";
-   let msge_encode = document.getElementById("message_encode");
-   msge_encode.innerHTML = `${encode}`;
+   document.getElementById('message_encode').value=cipher.encode(offset_value,encode);
+
+   /*let msge_encode = document.getElementById("message_encode");
+   msge_encode.innerHTML = `${encode}`;*/
    
 })
 
@@ -21,10 +25,15 @@ const button_decipher = document.getElementById("btn_decode");
 button_decipher.addEventListener('click',() =>{
    document.getElementById("box2").style.display="none";
    document.getElementById("box4").style.display="block";
+
+   let offset_value = document.getElementById('offset').value;
    let decode = document.getElementById('decodificar').value;
    document.getElementById('decodificar').value = "";
-   let msge_decode = document.getElementById("message_decode");
-   msge_decode.innerHTML = `${decode}`;
+   document.getElementById('message_decode').value=cipher.decode(offset_value,decode);
+
+   
+   /*let msge_decode = document.getElementById("message_decode");
+   msge_decode.innerHTML = `${decode}`;*/
 
 })
 
