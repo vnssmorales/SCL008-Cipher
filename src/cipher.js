f259a540dfae3313abe38fde5d0e8ba1e499fa0e
vnssmorales <vnss.morales@gmail.com>
2019-02-16 20:32:57 -0500

    hacker edition, corrijo errores en función que descifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,13 @@
 window.cipher = {
   encode: (offset_value,encode) => {
     let result ="";
+    let concatenar="";
 
     for (let i=0;i<encode.length;i++){
     
     let transform_text=encode.charCodeAt(i);
     if(transform_text>=65 && transform_text<=90){
-    let concatenar=String.fromCharCode((transform_text-65+parseInt(offset_value))%26+65);
+    concatenar=String.fromCharCode((transform_text-65+parseInt(offset_value))%26+65);
     result+= concatenar;
 
     }else if(transform_text=== 32){
@@ -21,6 +22,7 @@ window.cipher = {
     result+= concatenar;
 
     }else{
+      alert("cifrado terminado");
     }
     
     }
@@ -29,24 +31,26 @@ window.cipher = {
   
   decode: (value_offset,decode) => {
     let resultt ="";
+    let concatenarr="";
 
     for (let i=0;i<decode.length;i++){
 
     let transform_textd=decode.charCodeAt(i);
     if(transform_textd>=65 && transform_textd<=90){
-    let concatenarr=String.fromCharCode((transform_textd-90-parseInt(value_offset))%26+90);
+    concatenarr=String.fromCharCode((transform_textd-90-parseInt(value_offset))%26+90);
     resultt+= concatenarr;
 
     }else if(transform_textd=== 32){
     resultt+=" ";
     }else if(transform_textd>=33 && transform_textd<=63){
-      concatenarr=String-fromCharCode((transform_textd-63-parseInt(value_offset))%31+63);
+      concatenarr=String.fromCharCode((transform_textd-63-parseInt(value_offset))%31+63);
       resultt+=concatenarr;
     }else if(transform_textd>=97 && transform_textd<=122){
     concatenarr=String.fromCharCode((transform_textd-122-parseInt(value_offset))%26+122);
     resultt+= concatenarr;
 
     }else{
+      alert("cifrado terminado");
     }
     
     } 
