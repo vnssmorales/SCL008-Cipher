92b31f77d97a1daa96b16f11927c8e819fa28595
vnssmorales <vnss.morales@gmail.com>
2019-02-19 10:32:55 -0500

    test de cifrado y descifrado para minúsculas y letras

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -24,6 +24,41 @@ describe('cipher', () => {
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', ()=>{
       assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ" );
     });
+  });
+
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+
+    it('debería retornar "uvwxyzabcdefghijklmnopqrst" para "abcdefghijklmnopqrstuvwxyz" con offset 20', ()=>{
+      assert.equal(cipher.encode(20, "abcdefghijklmnopqrstuvwxyz"),"uvwxyzabcdefghijklmnopqrst");
+    });
+
+  });
+
+  describe('cipher.decode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+
+    it('debería retornar "mnopqrstuvcwxyzabcdefghijkl" para "abcdefghijqklmnopqrstuvwxyz" con offset 40', ()=>{
+      assert.equal(cipher.decode(40,"abcdefghijqklmnopqrstuvwxyz"),"mnopqrstuvcwxyzabcdefghijkl" );
+    });
+  });
+
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+
+    it('debería retornar "6789:;<=>?" para "0123456789" con offset 6', ()=>{
+      assert.equal(cipher.encode(6, "0123456789"),"6789:;<=>?");
+    });
+
+  });
   
-  });
-  });
+});
